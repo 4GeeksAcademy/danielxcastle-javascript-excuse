b37e72135606dbbfd09975fc1cdd3a8442766a0c
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-08-01 00:03:18 +0000

    final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function excuse() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const who = ["Ernesto", "My dog", "Lebron James", "My wife", "Jack", "Jill"];
+  const action = ["ripped up", "ate", "threw away", "ran over"];
+  const what = ["a cheeseburger", "a taco", "my homework", "my cat"];
+  const why = ["do not like me", "were late", "were hungry"];
+
+  const randomwhoIndex = Math.floor(Math.random() * who.length);
+  const randomactionIndex = Math.floor(Math.random() * action.length);
+  const randomwhatIndex = Math.floor(Math.random() * what.length);
+  const randomwhyIndex = Math.floor(Math.random() * why.length);
+
+  const phrase = `${who[randomwhoIndex]} ${action[randomactionIndex]} ${what[randomwhatIndex]} because they ${why[randomwhyIndex]}! `;
+
+  const targetheading = document.querySelector("#phrase");
+  targetheading.textContent = phrase;
+
+  console.log();
 };
