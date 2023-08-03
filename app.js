4993ecfb8443c791e6f378db5e5bef690fccb847
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-08-03 00:31:16 +0000

    refactored

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ window.onload = function excuse() {
   const why = ["do not like me", "were late", "were hungry"];
 
   const selectedWhoIndex = Math.floor(Math.random() * who.length);
-  const selectedcAtionIndex = Math.floor(Math.random() * action.length);
+  const selectedctionIndex = Math.floor(Math.random() * action.length);
   const selectedWhatIndex = Math.floor(Math.random() * what.length);
   const selectedWhyIndex = Math.floor(Math.random() * why.length);
 
