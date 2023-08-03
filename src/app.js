109f62c6eb7491f971c1c31a965c2c92b2b08fb5
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-08-03 00:18:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,22 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function excuse() {
-  //write your code here
-
   const who = ["Ernesto", "My dog", "Lebron James", "My wife", "Jack", "Jill"];
   const action = ["ripped up", "ate", "threw away", "ran over"];
   const what = ["a cheeseburger", "a taco", "my homework", "my cat"];
   const why = ["do not like me", "were late", "were hungry"];
 
-  const randomwhoIndex = Math.floor(Math.random() * who.length);
-  const randomactionIndex = Math.floor(Math.random() * action.length);
-  const randomwhatIndex = Math.floor(Math.random() * what.length);
-  const randomwhyIndex = Math.floor(Math.random() * why.length);
+  const selectedWhoIndex = Math.floor(Math.random() * who.length);
+  const selectedActionIndex = Math.floor(Math.random() * action.length);
+  const selectedWhatIndex = Math.floor(Math.random() * what.length);
+  const selectedWhyIndex = Math.floor(Math.random() * why.length);
 
-  const phrase = `${who[randomwhoIndex]} ${action[randomactionIndex]} ${what[randomwhatIndex]} because they ${why[randomwhyIndex]}! `;
+  const phrase = `${who[selectedWhoIndex]} ${action[selectedActionIndex]} ${what[selectedWhatIndex]} because they ${why[selectedWhyIndex]}! `;
 
-  const targetheading = document.querySelector("#phrase");
-  targetheading.textContent = phrase;
+  const selectedPhrase = document.querySelector("#phrase");
+  selectedPhrase.textContent = phrase;
 
   console.log();
 };
